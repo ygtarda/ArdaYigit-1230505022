ef1fd0f3438590363dac295a0a14968a8a4b0fa3
ardayigit <ardayig725@gmail.com>
2023-12-10 19:22:32 +0300

    Initial Commit

---
+++ ArdaYigit-1230505022/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ArdaYigit-1230505022
+//
+//  Created by Arda Yiğit on 10.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
